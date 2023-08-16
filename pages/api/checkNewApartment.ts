3a05578284928aec7e676ced996529dce4a2e63d
RamyEB <ramyelb@gmail.com>
2023-08-16 16:23:47 +0200

    restore to old commit

--- pages/api/checkNewApartment.ts
+++ pages/api/checkNewApartment.ts
@@ -1,16 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { postData } from "../../functions/postData";
+import { postData } from "@/functions/postData";
+import type { NextApiRequest, NextApiResponse } from "next";
+
 import nbApartment from "../../data/nbApartment.json";
 import filter from "../../data/filter.json";
-import pushNotification from "../../functions/pushNotification";
-import { NextApiRequest, NextApiResponse } from "next";
+import pushNotification from "@/functions/pushNotification";
 
 const { NTFY_URL, SELOGER_URL } = process.env;
 
 const SECRET_CHANNEL =
   process.env.NODE_ENV === "development"
-    ? process.env.SECRET_CHANNEL
-    : process.env.SECRET_CHANNEL_DEV;
+    ? process.env.SECRET_CHANNEL_DEV
+    : process.env.SECRET_CHANNEL;
+
+const url = process.env.URL;
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,23 +21,19 @@ export default async function handler(
 ) {
   try {
     const response = await postData(SELOGER_URL, filter);
-    await fetch(
-      `${NTFY_URL}${SECRET_CHANNEL}`,
-      pushNotification(response, nbApartment)
-    );
+
     if (req.query.reset) {
       nbApartment.lastCount = 0;
     } else if (nbApartment.lastCount !== response.nb) {
-      if (nbApartment.lastCount < response.nb && nbApartment.lastCount !== 0) {
+      if (nbApartment.lastCount < response.nb && nbApartment.lastCount !== 0)
         await fetch(
           `${NTFY_URL}${SECRET_CHANNEL}`,
           pushNotification(response, nbApartment)
         );
-      }
       nbApartment.lastCount = response.nb;
     }
-    res.json({ message: "Good!", nbApartment, response });
-  } catch (err: any) {
-    throw new Error(err.message);
+    res.status(200).json({ message: "Good!", nbApartment, response });
+  } catch (err) {
+    res.json({ error: "failed to load data" });
   }
 }
